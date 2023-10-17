395b4ddc109583943fae2ccb7f0c66c10d3c4031
mclements20 <mclements20@georgefox.edu>
2023-10-16 19:54:26 -0700

    changes were made
    changes were made

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggleVisibility(id) {
+    let element = document.getElementById(id);
+    if (element.style.display === 'none' || element.style.display === '') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
