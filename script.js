300fa5d09bcc039c68c7b16b7ff40baf83da3639
mclements20 <mclements20@georgefox.edu>
2023-11-09 15:26:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,41 +9,8 @@ function toggleVisibility(id) {
 }
 
 
-// Initialize the secret code
-const secretCode = generateSecretCode();
-
-// Function to generate a random secret code
-function generateSecretCode() {
-    let code = [];
-    for (let i = 0; i < 4; i++) {
-        code.push(Math.floor(Math.random() * 10)); // Use numbers instead of colors
-    }
-    return code;
-}
-
-// Function to check the guess and update the results
-function checkGuess(guess) {
-    // Convert the guess to an array of numbers
-    const guessNumbers = guess.map(Number);
-
-    // Check if the input is valid (4 numbers)
-    if (guessNumbers.length === 4 && guessNumbers.every(num => num >= 0 && num <= 9)) {
-        // Implementation of code checking logic (compare guessNumbers with the secretCode)
-        // Update the results dynamically on the page
-        // Add animation if the code is cracked
-    } else {
-        alert('Please enter 4 numbers between 0 and 9.');
-    }
-}
-
-// Function to handle user input and update the page
-function submitGuess() {
-    const guessInput = $('#guess').val().split(' ').map(Number);
-    checkGuess(guessInput);
-}
-
 // Initial setup - add animation, etc.
-$(document).ready(function() {
+$(document).ready(function () {
     let rocket = $('#rocket');
     let hintButton = $('#hint-btn');
     let revealedCount = 0;
@@ -54,29 +21,46 @@ $(document).ready(function() {
         rocket.animate({
             marginTop: '-500px',
             opacity: '0'
-        }, 2000, function() {
+        }, 2000, function () {
             // Reset rocket position after animation
             rocket.css({ marginTop: '0', opacity: '1' });
         });
     }
 
+    // Function to generate a random secret code
+    function generateSecretCode() {
+        let code = [];
+        for (let i = 0; i < 4; i++) {
+            code.push(Math.floor(Math.random() * 10));
+        }
+        return code;
+    }
+
+    // Initialize the secret code
+    const secretCode = generateSecretCode();
+
     // Function to check the guess and update the results
     function checkGuess(guess) {
-        // Implementation of code checking logic (compare guess with the secretCode)
-        // Update the results dynamically on the page
+        // Check if the input is valid (4 numbers)
+        if (guess.length === 4 && guess.every(num => num >= 0 && num <= 9)) {
+            // Implementation of code checking logic (compare guess with the secretCode)
+            // Update the results dynamically on the page
 
-        // Check if the code is cracked
-        if (codeIsCracked) {
-            // Add rocket launch animation
-            launchRocket();
+            // Check if the code is cracked
+            if (codeIsCracked) {
+                // Add rocket launch animation
+                launchRocket();
+            } else {
+                // Add incorrect guess to missed attempts
+                missedAttempts.push(guess.join(' '));
+                updateMissedAttempts();
+            }
+
+            // Update the guesses container
+            updateGuesses(guess);
         } else {
-            // Add incorrect guess to missed attempts
-            missedAttempts.push(guess.join(' '));
-            updateMissedAttempts();
+            alert('Please enter 4 numbers between 0 and 9.');
         }
-
-        // Update the guesses container
-        updateGuesses(guess);
     }
 
     // Function to update the guesses list on the page
@@ -91,7 +75,7 @@ $(document).ready(function() {
         missedList.empty(); // Clear previous attempts
 
         // Append new attempts to the list
-        missedAttempts.forEach(function(attempt) {
+        missedAttempts.forEach(function (attempt) {
             $('<li>').text(attempt).appendTo(missedList);
         });
     }
@@ -105,22 +89,9 @@ $(document).ready(function() {
     // Function to reveal one number as a hint
     function revealHint() {
         if (revealedCount < 4) {
-            // Replace this logic with the actual hint implementation
             alert('Hint: ' + secretCode[revealedCount]);
             revealedCount++;
 
             // Check if all numbers are revealed
             if (revealedCount === 4) {
-                // Reset the game or perform any other actions
-                alert('All numbers revealed. Game reset!');
-                // Add code to reset the game
-            }
-        }
-    }
-
-    // Attach click event to the submit button
-    $('#submit-btn').click(submitGuess);
-
-    // Attach click event to the hint button
-    hintButton.click(revealHint);
-});
+                alert('All numbers revealed. Game
